1a4c7222302fe5a3f66e8a40e200d4a82843ee2f
Tobi Adeyinka <tobex9434@yahoo.com>
2017-02-05 18:08:04 +0100

    setup main activity menu

--- app/src/main/java/com/tobiadeyinka/popularmovies/activities/MainActivity.java
+++ app/src/main/java/com/tobiadeyinka/popularmovies/activities/MainActivity.java
@@ -3,12 +3,59 @@ package com.tobiadeyinka.popularmovies.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.tobiadeyinka.popularmovies.R;
+
+/**
+ * @author Tobi Adeyinka
+ */
 
 public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.popularity:
+                return sortByPopularity();
+            case R.id.rating:
+                return sortByRating();
+            default:
+                return false;
+        }
+    }
+
+    /*
+     * Sort movies by popularity
+     */
+    private boolean sortByPopularity(){
+        Toast.makeText(getApplicationContext(), "sort by popularity", Toast.LENGTH_SHORT).show();
+        return true;
+    }
+
+    /*
+     * Sort movies by rating
+     */
+    private boolean sortByRating(){
+        Toast.makeText(getApplicationContext(), "sort by rating", Toast.LENGTH_SHORT).show();
+        return true;
     }
 
 }
--- app/src/main/java/com/tobiadeyinka/popularmovies/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/tobiadeyinka/popularmovies/activities/MovieDetailsActivity.java
@@ -2,5 +2,9 @@ package com.tobiadeyinka.popularmovies.activities;
 
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * @author Tobi Adeyinka
+ */
+
 public class MovieDetailsActivity extends AppCompatActivity {
 }
